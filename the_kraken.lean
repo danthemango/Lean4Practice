b584816c271daab611130f2f9fb03c4d5c02309c
daniel guenther <danthemango@gmail.com>
2026-08-24 16:06:53 -0700

    add some more kraken

--- the_kraken.lean
+++ the_kraken.lean
@@ -8,10 +8,26 @@ theorem or_inr {P Q} (p : P) : Q ∨ P := by
   right
   assumption
 
--- the kraken
 example (O S : Prop)(s : S) : S ∨ O := by
   have h4 : S ∨ O := or_inl s
   exact h4
 
-example (O S : Prop)(s : S) : K ∨ S := by
+-- simpler
+example (O S : Prop)(s : S) : S ∨ O := by
+  exact or_inl s
+
+example (K S : Prop)(s : S) : K ∨ S := by
+  exact or_inr s
+
+theorem or_elim {P Q R} (h : P ∨ Q) (_ : P → R) (_ : Q → R)
+  : Q ∨ P
+  := by
+  cases h
+  right
+  assumption
+  left
+  assumption
+
+example (B C I : Prop)(h1 : C → B)(h2 : I → B)(h3 : C ∨ I) : B := by
+  -- exact or_elim h3 h1 h2
   sorry
