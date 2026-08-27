8d1c1ed9e3aef1cdab5ad3733a0894c0c2d8ad21
Daniel Guenther <Daniel.Guenther@nanaimo.ca>
2026-08-27 13:10:02 -0700

    complete implies a negation

--- lean4game-logic/Falsification.lean
+++ lean4game-logic/Falsification.lean
@@ -1,13 +1,14 @@
 import Lean4PracticeWithMathlib.Basic
 
+-- and introduction
+example (P S : Prop)(p: P)(s : S) : P ∧ S := by
+  exact and_intro p s
+
 theorem false_elim {P : Prop} (h : False) : P := by
   have h2 (x : Prop) : False -> x := by
     intro p
     contradiction
   exact h2 P h
-
--- theorem and_intro {P Q} (left : P) (right : Q) : P ∧ Q := by
-
 
 -- not s means s implies false
 
@@ -32,14 +33,17 @@ example (P : Prop)(p : P) : ¬¬P := by
 -- The law of non-self-contradiction
 example (L : Prop) : ¬(L ∧ ¬L) := by
   have h1 : ¬L → L → False := λ h => h
-  have h2 : L ∧ ¬L → False := λ hb => (h1 hb.right) hb.left
+  have h2 : L ∧ ¬L → False := λ hlnl => (h1 hlnl.right) hlnl.left
   exact h2
 
 -- Modus Tollens.
-example (B S : Prop)(h1 : B → S)(h2 : ¬S) : ¬B := by
+theorem modus_tollens (B S : Prop) (h1 : B → S) (h2 : ¬S) : ¬B := by
   -- exact λ hb => h2 (h1 hb)
   -- using function composition
   exact λ hb => (h2 ∘ h1) hb
+
+theorem modus_ponens (B S : Prop) (h1 : B → S) (h2 : B) : S := by
+  exact h1 h2
 
 example (A : Prop) (h: A → ¬A) : ¬A := by
   have h2 : (A → ¬A) → A → ¬A:= λ h => h
@@ -48,16 +52,40 @@ example (A : Prop) (h: A → ¬A) : ¬A := by
   have h5 : ¬A := λ ha => (h4 ha) h
   exact h5
 
+-- Negation
 -- Nested λ↦s.
 example (B C : Prop) (h: ¬(B → C)) : ¬C := by
-  -- have h7 : (B → C) → ¬C := λ lbc => false_elim (h lbc)
-  -- have h9 : (B → C) → False → ¬C := λ hbc => false_elim (h hbc)
-  -- have h11 : ¬(B → C) → (B → C) → False → ¬C := λ hbc =>
-  -- have h10 : ¬(B → C) → ¬ C := λ nbc => (h nbc)
-  -- have h8 : ¬C := (h3 h)
+  -- ¬(B → C) ↔ ¬(¬B ∨ C) ↔ B ∧ ¬C
+  -- have h1 : ¬(B → C) → B ∧ ¬C :=
+
+  -- have h1 : B → C → (B ∧ C) := λ hb => λ hc => and_intro hb hc
+  -- have h2 : (B → C) → (B ∧ C) := λ hbc => false_elim (h hbc)
+  -- have h3 : (B → C) → ¬C := λ hbc => λ nc => false_elim (h hbc)
 
   have the_goal : ¬C := by sorry
   exact the_goal
+
+-- Negation into conjuction.
+example (C S : Prop) (s: S) : ¬(¬S ∧ C) := by
+  -- ¬(¬S ∧ C) ↔ S ∨ ¬C
+  -- S → (S ∨ ¬C)
+  sorry
+
+-- implies a negation
+example (A P : Prop) (h : P → ¬A) : ¬(P ∧ A) := by
+  have h4 : ¬A → A → False := λ h => h
+  have h3 : A ∧ ¬A → False := λ hana => (h4 hana.right) hana.left
+  have h17 : (P → ¬A) → (P ∧ A) → (A ∧ ¬A) := λ pna => λ npa => and_intro npa.right (pna npa.left)
+  have h18 : (P → ¬A) → ¬(P ∧ A) := λ pna => λ npa => h3 ((h17 pna) npa)
+  exact h18 h
+
+-- Conjunction Implication
+example (A P : Prop) (h: ¬(P ∧ A)) : P → ¬A := by
+  sorry
+
+
+example (C S : Prop) (s: S) : S ∨ C := by
+  sorry
 
 
 -- TODO more problems incompleted
