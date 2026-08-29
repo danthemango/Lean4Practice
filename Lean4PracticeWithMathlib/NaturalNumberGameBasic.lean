b1be75832172f7f3296c60a772369dbf5382d4a1
daniel guenther <danthemango@gmail.com>
2026-08-28 18:36:46 -0700

    add some natural number game

---
+++ Lean4PracticeWithMathlib/NaturalNumberGameBasic.lean
@@ -0,0 +1,13 @@
+
+
+theorem succ_inj (a b : Nat) (h : Nat.succ a = Nat.succ b) : a = b := by
+  sorry
+
+theorem succ_eq_add_one (n : Nat) : Nat.succ n = n + 1 := by
+  sorry
+
+theorem zero_ne_succ (a : Nat) : 0 ≠ Nat.succ a := by
+  sorry
+
+theorem one_eq_succ_zero : 1 = Nat.succ 0 := by
+  sorry
---
+++ Natural Number Game/Implication World.lean
@@ -0,0 +1,36 @@
+import Lean4PracticeWithMathlib.NaturalNumberGameBasic
+
+-- note: this game will attempt to not use Nat
+-- but i'll just use Nat to get used to it
+
+-- x+1=y+1⟹x=y.
+example (x y : Nat) : x + 1 = y + 1 → x = y := by
+  intro h
+  rw [← succ_inj x y]
+  rw [succ_eq_add_one]
+  rw [succ_eq_add_one]
+  exact h
+
+-- If x=y and x  =y then we can deduce a contradiction.
+example (x y : Nat) (h1 : x = y) (h2 : x ≠ y) : False := by
+  apply h2
+  exact h1
+
+-- Theorem zero_ne_one: 0  =1.
+theorem zero_ne_one : (0 : Nat) ≠ 1 := by
+  intro h
+  rw [one_eq_succ_zero] at h
+  apply zero_ne_succ 0
+  exact h
+
+-- Theorem one_ne_zero: 1  =0.
+theorem one_ne_zero : (1 : Nat) ≠ 0 := by
+  symm
+  exact zero_ne_one
+
+-- 2+2  =5.
+example : Nat.succ (Nat.succ 0) + Nat.succ (Nat.succ 0) ≠ Nat.succ (Nat.succ (Nat.succ (Nat.succ (Nat.succ 0)))) := by
+  intro h
+  -- rw [succ_inj] at h
+
+  sorry
